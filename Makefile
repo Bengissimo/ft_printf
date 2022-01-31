51053872339306efc8cd3cbcdee5bb747dd0959e
Bengisu Kandemir <bkandemi@student.hive.fi>
2022-01-31 15:12:57 +0200

    Makefile added, test folder added and makefile tested with a simple ft_printf.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bkandemi <bkandemi@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/01/31 11:00:37 by bkandemi          #+#    #+#              #
-#    Updated: 2022/01/31 11:38:58 by bkandemi         ###   ########.fr        #
+#    Updated: 2022/01/31 15:09:45 by bkandemi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,27 +18,21 @@ O_FILES = $(C_FILES:.c=.o)
 
 CC = gcc
 
-FLAGS = -Wall -Wextra -Werror
-
-INCLUDES = -I . -I libft -L libft -lft -lftprintf
+FLAGS = -c -Wall -Wextra -Werror
 
 all : $(NAME)
 
 $(NAME):
-		@make -C libft/ fclean && make -C libft/
-		@$(CC) $(FLAGS) -c $(C_FILES) 
+		@$(CC) $(FLAGS) $(C_FILES) 
 		@ar rc $(NAME) $(O_FILES)
 		
 clean:
-	@make clean -C libft
 	@rm -f $(O_FILES)
  
 fclean: clean
-	@make fclean -C libft
 	@rm -f $(NAME)
 
 re:	fclean all
 
-test:
 	
 
---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkandemi <bkandemi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 14:31:19 by bkandemi          #+#    #+#             */
+/*   Updated: 2022/01/31 14:41:41 by bkandemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+#define TRUE 1
+#define FALSE 0
+
+int	ft_printf(char *str);
+
+
+#endif
